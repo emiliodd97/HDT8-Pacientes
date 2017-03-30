ab0ef78863cc1151d3d0f8cdd14e22f937006971
samanthaduarte <dua16256@uvg.edu.gt>
2017-03-30 11:47:04 -0600

    Clases .java

---
+++ Paciente.java
@@ -0,0 +1,13 @@
+import java.io.IOException;
+
+public class Paciente {
+
+	public Paciente() throws IOException {
+		// TODO Auto-generated constructor stub
+		Texto texto = new Texto();
+		String pacientes = texto.leerTexto();
+		
+		
+	}
+
+}
